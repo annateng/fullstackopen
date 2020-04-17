a9c298a2839ef6071a515fa2d6a1287611e1eccd
annateng <anna.w.teng@gmail.com>
2020-04-17 01:18:18 -0400

    updates to phonebook through 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ({searchVal, handleChange}) => (
   <div>
@@ -29,15 +30,18 @@ const Persons = ({persons, searchVal}) => (
 )
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ searchVal, setSearchVal ] = useState('')
+
+  useEffect(
+    () => {axios
+            .get('http://localhost:3001/persons')
+            .then(response => setPersons(response.data))
+          },
+    []
+  )
 
   const handleNameChange = (event) => setNewName(event.target.value)
   const handleNumberChange = (event) => setNewNumber(event.target.value)
